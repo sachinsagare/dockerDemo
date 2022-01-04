4b0d505532fe75f0fe07be624a06f2dbea5ce346
sachinsagare <96275911+sachinsagare@users.noreply.github.com>
2022-01-04 13:19:24 -0500

    Add files via upload

---
+++ src/main/java/com/example/dockerDemo/DockerDemoApplication.java
@@ -0,0 +1,22 @@
+package com.example.dockerDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DockerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDemoApplication.class, args);
+	}
+	
+	@GetMapping("/")
+	public String app() {
+		return "Docker demo App";
+		
+	}
+
+}
